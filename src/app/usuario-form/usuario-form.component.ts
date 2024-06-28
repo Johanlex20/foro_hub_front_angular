f3e396f15e57e00dada8215433c9258fa78d38f5
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-06-27 19:18:08 -0500

    METODO SAVE, ESTRUCTURACION VALIDACIONES FUNCION CONTROLHSERROR, MODIFIACON FORMULARIO

--- src/app/usuario-form/usuario-form.component.ts
+++ src/app/usuario-form/usuario-form.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsuarioService } from '../usuario-list/usuario.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-usuario-form',
@@ -11,17 +13,38 @@ export class UsuarioFormComponent {
     nombre:['',[Validators.required, Validators.minLength(3), Validators.maxLength(45)]],
     email:['', [Validators.required, Validators.email]],
     password:[,[Validators.required, Validators.pattern('[a-z0-9- ]+'), Validators.minLength(5)]],
-    role:[,[]]
+    role:[,[Validators.required]]
   })
 
   constructor(
-    private fb : FormBuilder
+    private fb : FormBuilder,
+    private usuarioService: UsuarioService,
+    private router: Router
   ){}
 
+  controlHasError(control: string, error: string){
+    return this.form.controls[control].hasError(error);
+  };
+
   save(){
-    console.log('form valid' , this.form.valid);
-    console.log('form', this.form.value);
+    if(this.form.invalid){
+      return;
+    }
+
+    let usuario = this.form.value;
+    usuario.filePerfil = "img.dumy"
+
+    this.usuarioService.create(usuario)
+      .subscribe( usuario=>{
+        this.router.navigate(['/usuarios']);
+      });
+  
   }
+
+
+
+  
+  
 
 
 }
--- src/app/usuario-list/usuario.service.ts
+++ src/app/usuario-list/usuario.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { UsuarioPage } from '../interfaces/usuairo.interface';
+import { Usuario, UsuarioPage } from '../interfaces/usuairo.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +14,10 @@ export class UsuarioService {
 
   pagiante(){
     return this.http.get<UsuarioPage>('http://localhost:8080/api/usuario');
+  }
+
+  create(usuario: Usuario){
+    return this.http.post<Usuario>('http://localhost:8080/api/usuario', usuario);
   }
 
 }

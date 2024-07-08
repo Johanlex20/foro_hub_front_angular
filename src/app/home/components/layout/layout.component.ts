3fccacbf19f49ce484f20fdd70434ebd1ca73a1c
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-07-08 17:53:21 -0500

    MODIFICAMOS BOTONES HTML LOGING, LOGOUT  Y ADMINISTRACION POR ROLES

--- src/app/home/components/layout/layout.component.ts
+++ src/app/home/components/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-layout',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: []
 })
 export class LayoutComponent {
+
+  constructor(
+    private authService: AuthService
+  ){}
+
+
+  get usuario(){
+    return this.authService.usuario;
+  }
+
+
+  logout(){
+    this.authService.logout();
+  }
 
 }

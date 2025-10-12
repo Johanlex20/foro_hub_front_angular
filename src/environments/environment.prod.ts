f1bfe6e0d690e89544d3a3de76149c59e931c66c
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2025-10-12 15:43:59 -0500

    Variables de produccion ajustes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiBase: 'https://www.Good-Company.com/api',
+    apiBase: 'http://forohub_backend_spring:8080/api'
     //paypalReturnUrl: 'http://localhost:4200/cart'
 
 }

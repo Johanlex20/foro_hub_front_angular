16456f523a43764b31834232bab3d4a16795ccc1
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2025-10-12 20:06:40 -0500

    Ajuste ruta

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
    // apiBase: 'http://localhost:8080/api',
-   apiBase: 'http://192.168.20.103:8080/api',
+   //apiBase: 'http://192.168.20.103:8080/api',
+   apiBase:'https://apiforogood.modoblu.com/api',
 
 }

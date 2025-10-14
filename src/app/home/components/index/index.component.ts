51bec7525e110447de6a42ccbfd5aa6cb558e620
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2025-10-13 20:37:00 -0500

    agregando libreria Swiper para el carrusel de temas en el home

--- src/app/home/components/index/index.component.ts
+++ src/app/home/components/index/index.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef} from '@angular/core';
 import { HomeService } from '../../services/home.service';
 import { Tema } from 'src/app/interfaces/tema.interface';
 import { Usuario } from 'src/app/interfaces/usuairo.interface';
 import { AuthService } from '../../../auth/services/auth.service';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
+
+import Swiper, { Autoplay, Navigation } from 'swiper';
+Swiper.use([Autoplay, Navigation]);
 
 @Component({
   selector: 'app-index',
@@ -14,12 +17,14 @@ import Swal from 'sweetalert2';
 export class IndexComponent implements OnInit{
 
   lastTemas?: Tema[];
+  swiperInitialized = false;
   //usuario: Usuario;
 
   constructor(
     private homeService: HomeService,
     private authService:AuthService,
-    private router:Router
+    private router:Router,
+    private cdr: ChangeDetectorRef
   ){}
 
   ngOnInit(): void {
@@ -48,6 +53,34 @@ export class IndexComponent implements OnInit{
       timer: 2500
     });
   }
+
+ngAfterViewInit(): void {
+  setTimeout(() => {
+    if (this.lastTemas && this.lastTemas.length > 0 && !this.swiperInitialized) {
+      const swiper = new Swiper('.mySwiper', {
+        slidesPerView: 5,
+        spaceBetween: 20,
+        loop: true,
+        grabCursor: true,
+        speed: 800,
+        autoplay: {
+          delay: 1800,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: false,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        observer: true,
+        observeParents: true,
+      });
+      swiper.autoplay.start(); // ✅ Reinicia si se pausa
+      this.swiperInitialized = true;
+      this.cdr.detectChanges();
+    }
+  }, 300);
+}
 
 
 }

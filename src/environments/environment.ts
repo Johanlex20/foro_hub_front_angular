713bf2df118d6b276aa1fafb0d28dcfb06b61dfe
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2025-10-25 19:21:14 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
+    production: true,
     //apiBase: 'http://localhost:8080/api',
    //apiBase: 'http://192.168.20.103:8080/api',
    apiBase:'https://apiforogood.modoblu.com/api',
